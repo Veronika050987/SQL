c9b5c3cf2aa052c8f04160aef9702fff83d8661d
Veronika050987 <veronicaviv@mail.ru>
2025-09-01 22:30:19 +0300

    DML

---
+++ DML/SQLQuery1-INSERT.sql
@@ -0,0 +1,33 @@
+USE PD_411_SQL;
+GO
+--	Directions
+--INSERT Directions
+--		(direction_id, directio_name)
+--VALUES (1,N'Разработка программного обеспечения');
+
+--INSERT Directions
+--VALUES (2, N'Сетевые технологии и системное администирирование');
+
+--INSERT Directions
+--		(directio_name, direction_id)
+--VALUES	(N'Компьютерная графика и дизайн', 3);
+
+--GO
+
+--SELECT * FROM Directions;
+
+
+----------------------------
+--- Group:
+--INSERT Groups
+--		(group_id, group_name, direction)
+--VALUES
+--		(1, N'PU_211',1),
+--		(20, N'PV_211',1),
+--		(21, N'PD_321',1),
+--		(22, N'PD_212',1),
+--		(24, N'PD_411',1),
+--		(25, N'VPD_311',1)
+--		;
+
+--SELECT * FROM Groups;
---
+++ DML/SQLQuery2-SELECT.sql
@@ -0,0 +1,33 @@
+--SQLQuery2.sql
+USE PD_411_SQL;
+GO
+
+--SELECT columns FROM tables;
+
+--SELECT	
+--		direction_id		AS	N'Номер направления',
+--		directio_name		AS	N'Название'
+--FROM	Directions;
+
+--INSERT	Groups
+--		(group_id,group_name,direction)
+--VALUES	(34,N'SU_511',2);
+
+--SELECT
+--		group_name			AS	N'Название группы',
+--		directio_name		AS	N'Направление обучения'
+--FROM	Groups,Directions
+--WHERE	direction	=	direction_id
+--AND		direction	LIKE(N'Сетевые технологи%')
+--;
+--% - все возможные символы
+
+SELECT
+		last_name + '' + first_name + '' + middle_name AS N'Студент',
+		group_name AS N'Группа',
+		directio_name AS N'Направление обучения'
+FROM	Students,Groups,Directions
+WHERE	[group]		= group_id
+AND		direction	= direction_id
+;
+
