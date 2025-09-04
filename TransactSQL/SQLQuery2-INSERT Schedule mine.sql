7efb17420ea12afd25e47f8d6ac0a8f933024f36
Veronika050987 <veronicaviv@mail.ru>
2025-09-05 00:06:32 +0300

    .table schedule done

--- TransactSQL/SQLQuery2-INSERT Schedule mine.sql
+++ TransactSQL/SQLQuery2-INSERT Schedule mine.sql
@@ -1,4 +1,4 @@
---SQLQuery1-INSERT Schedule.sql
+--SQLQuery1-INSERT Schedule mine.sql
 USE PD_321;
 GO
 
@@ -7,6 +7,8 @@ SET DATEFIRST 1;
 DECLARE		@start_date			AS		DATE	=	'2025-08-18';
 DECLARE		@date				AS		DATE	=	@start_date;
 DECLARE		@start_time			AS		TIME	=	'13:30';
+DECLARE		@start_time1		AS		TIME	=	'15:05';
+
 
 DECLARE		@discipline			AS		SMALLINT=	(SELECT	discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'%MS SQL SERVER%');
 DECLARE		@lessons_count		AS		TINYINT	=	(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_name LIKE N'%MS SQL SERVER%');
@@ -23,14 +25,32 @@ PRINT		('========================================');
 DECLARE		@lesson_number		AS		INT		=	1;
 
 WHILE(@lesson_number < @lessons_count)
-BEGIN
+BEGIN		
 		PRINT(@date);
+		
 		PRINT(DATENAME(WEEKDAY, @date));
+		
 		PRINT(@lesson_number);
+
 		PRINT(@start_time);
+		INSERT INTO Schedule (lesson_id, [group], discipline, teacher, [date], [time]) 
+		    VALUES (@lesson_number, @group, @discipline, @teacher, @date, @start_time);
+		
 		SET @lesson_number = @lesson_number+1;
 		PRINT(DATEADD(MINUTE, 95, @start_time));
+
+		INSERT INTO Schedule (lesson_id, [group], discipline, teacher, [date], [time]) 
+		    VALUES (@lesson_number, @group, @discipline, @teacher, @date, @start_time1);		
+		
 		SET @lesson_number = @lesson_number+1;
 		SET @date = DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date)=5,3,2), @date);
 		PRINT('----------------------------------------------------------')
+		
+		
+		--INSERT INTO Schedule (lesson_id, [group], discipline, teacher, [date], [time]) 
+		--    VALUES (@lesson_number, @group, @discipline, @teacher, @date, @start_time1);
+
+
 END
+
+SELECT * FROM Schedule
--- TransactSQL/SQLQuery3-INSERT Schedule PD_411.sql
+++ TransactSQL/SQLQuery3-INSERT Schedule PD_411.sql
@@ -1,12 +1,14 @@
---SQLQuery1-INSERT Schedule.sql
+--SQLQuery3-INSERT Schedule PD_411.sql
 USE PD_321;
 GO
 
 SET DATEFIRST 1;
 
-DECLARE		@start_date			AS		DATE	=	'2025-08-18';
+DECLARE		@start_date			AS		DATE	=	'2025-01-27';
 DECLARE		@date				AS		DATE	=	@start_date;
 DECLARE		@start_time			AS		TIME	=	'13:30';
+DECLARE		@start_time1		AS		TIME	=	'15:05';
+
 
 DECLARE		@discipline			AS		SMALLINT=	(SELECT	discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'%MS SQL SERVER%');
 DECLARE		@lessons_count		AS		TINYINT	=	(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_name LIKE N'%MS SQL SERVER%');
@@ -23,14 +25,27 @@ PRINT		('========================================');
 DECLARE		@lesson_number		AS		INT		=	1;
 
 WHILE(@lesson_number < @lessons_count)
-BEGIN
+BEGIN		
 		PRINT(@date);
+		
 		PRINT(DATENAME(WEEKDAY, @date));
+		
 		PRINT(@lesson_number);
+
 		PRINT(@start_time);
+		--INSERT INTO Schedule (lesson_id, [group], discipline, teacher, [date], [time]) 
+		--    VALUES (@lesson_number, @group, @discipline, @teacher, @date, @start_time);
+		
 		SET @lesson_number = @lesson_number+1;
 		PRINT(DATEADD(MINUTE, 95, @start_time));
+
+		--INSERT INTO Schedule (lesson_id, [group], discipline, teacher, [date], [time]) 
+		--    VALUES (@lesson_number, @group, @discipline, @teacher, @date, @start_time1);		
+		
 		SET @lesson_number = @lesson_number+1;
 		SET @date = DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date)=5,3,2), @date);
 		PRINT('----------------------------------------------------------')
+		
 END
+
+SELECT * FROM Schedule
