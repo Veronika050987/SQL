f3a2d1c54b9e1bc804b4f8e479e1102a3fe94d8a
Veronika050987 <veronicaviv@mail.ru>
2025-09-15 19:30:56 +0300

    .trigger

--- TransactSQL/SQLQuery0-Calling Procedures.sql
+++ TransactSQL/SQLQuery0-Calling Procedures.sql
@@ -4,15 +4,17 @@ GO
 
 --DELETE FROM Schedule;
 --SELECT * FROM Schedule; 
-EXEC sp_AddSchedule N'PD_411', N'%MS SQL Server%', N'Ковтун', '2024-12-23', '13:30';
-EXEC sp_AddSchedule N'PD_411', N'%ADO.NET%', N'Ковтун';
-EXEC sp_AddSchedule N'PD_411', N'Системное программирование%', N'Ковтун';
-EXEC sp_AddSchedule N'PD_411', N'Сетевое программирование%', N'Ковтун', '2025-08-18', '13:30';
-EXEC sp_AddSchedule N'PD_411', N'HTML/CSS%', N'Ковтун', '2025-08-18', '13:30';
-EXEC sp_SelectSchedule;
-EXEC sp_InsertIntoCompleteDisciplines;
+--EXEC sp_AddSchedule N'PD_411', N'%MS SQL Server%', N'Ковтун', '2024-12-23', '13:30';
+--EXEC sp_AddSchedule N'PD_411', N'%ADO.NET%', N'Ковтун';
+--EXEC sp_AddSchedule N'PD_411', N'Системное программирование%', N'Ковтун';
+--EXEC sp_AddSchedule N'PD_411', N'Сетевое программирование%', N'Ковтун', '2025-08-18', '13:30';
+--EXEC sp_AddSchedule N'PD_411', N'HTML/CSS%', N'Ковтун', '2025-08-18', '13:30';
+--EXEC sp_SelectSchedule;
+--EXEC sp_InsertIntoCompleteDisciplines;
 
-PRINT(dbo.GetNextLearningDay(N'PD_411', NULL));
-PRINT(N'-----------------------------');
-PRINT(dbo.IsLearningDay(N'PD_411', '2025-09-12'));
-PRINT(dbo.GetNextLearningDay(N'PD_411', DEFAULT));
+--PRINT(dbo.GetNextLearningDay(N'PD_411', NULL));
+--PRINT(N'-----------------------------');
+--PRINT(dbo.IsLearningDay(N'PD_411', '2025-09-12'));
+--PRINT(dbo.GetNextLearningDay(N'PD_411', DEFAULT));
+
+EXEC	dbo.sp_SelectSchedule;
